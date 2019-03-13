0148c1046814aa0d2ddd971ace982990d7190aee
jeongpyoHong <hjp6223@naver.com>
2019-03-13 15:03:22 +0900

    1.product 생성 2.서브 메뉴 밑에 자동으로 정렬되게 for 문과 if 문 사용해서 생성

--- smweb/web/urls.py
+++ smweb/web/urls.py
@@ -5,4 +5,5 @@ app_name='web'
 
 urlpatterns=[
     path('',views.index, name='index'),
+    path('',views.chart, name='chart')
 ]
--- smweb/web/views.py
+++ smweb/web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from web.menu.models import Mainmenu,Submenu
+from web.menu.models import Mainmenu,Submenu,Product
 from django.http import HttpResponseRedirect
 from django.urls import  reverse
 # Create your views here.
@@ -7,10 +7,13 @@ from django.urls import  reverse
 def index(request):
     mainMenu_Name_list=Mainmenu.objects.all()[:10]
     subMenu_Name_list=Submenu.objects.all()[:10]
-    context ={'mainMenu_Name_list':mainMenu_Name_list,'subMenu_Name_list':subMenu_Name_list}
+    prodouct_list=Product.objects.all()[:10]
+    context ={'mainMenu_Name_list':mainMenu_Name_list,'subMenu_Name_list':subMenu_Name_list,'product_list':prodouct_list}
     return render(request,'web/index.html',context)
 
 def stock(request,mainmenuName_id):
     mainmenu=get_object_or_404(Mainmenu,pk=mainmenuName_id)
     return render(request,'web/stock.html',{'mainmenu':mainmenu})
 
+def chart(request):
+    return render(request,'web/chart.html')
